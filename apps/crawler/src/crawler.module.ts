cfffc459053e777c462c6f1b5f4f7487a21e26e9
Boris Belobrov <vilgeforc5@gmail.com>
2025-09-27 21:47:32 +0300

    add redis provider to crawler.module.ts

--- apps/crawler/src/crawler.module.ts
+++ apps/crawler/src/crawler.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CrawlerController } from './crawler.controller';
 import { CrawlerService } from './crawler.service';
+import { RedisModule } from '@app/redis';
 
 @Module({
+  imports: [RedisModule],
   controllers: [CrawlerController],
   providers: [CrawlerService],
 })
