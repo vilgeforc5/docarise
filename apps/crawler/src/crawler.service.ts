ffaf224826b2aaeed038e9c68604bba6275f579a
Boris Belobrov <vilgeforc5@gmail.com>
2025-09-28 23:11:41 +0300

    remove crawlee lib .

--- apps/crawler/src/crawler.service.ts
+++ apps/crawler/src/crawler.service.ts
@@ -1,25 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { CrawlDto } from '@app/redis';
-import { PuppeteerCrawler, RequestQueue } from 'crawlee';
 
 @Injectable()
 export class CrawlerService {
   constructor() {}
 
-  async crawl({ url, operationId }: CrawlDto) {
-    const requestQueue = await RequestQueue.open(operationId + Math.random());
-
-    const crawler = new PuppeteerCrawler({
-      requestQueue,
-      async requestHandler({ request, page, enqueueLinks, log }) {
-        const title = await page.title();
-        log.info(`Title of ${request.loadedUrl} is '${title}'`);
-
-        await enqueueLinks();
-      },
-    });
-
-    await requestQueue.addRequest({ url });
-    await crawler.run();
+  crawl({ url, operationId }: CrawlDto) {
+    console.log(url, operationId);
   }
 }
